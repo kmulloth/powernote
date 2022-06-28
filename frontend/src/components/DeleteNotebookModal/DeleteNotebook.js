ccbaf569f045c72685cc6b4c7c2292704f44fbd8
KMulloth <k.mulloth@gmail.com>
2022-06-28 11:31:13 -0500

    3/4 crud for notebooks

--- frontend/src/components/DeleteNotebookModal/DeleteNotebook.js
+++ frontend/src/components/DeleteNotebookModal/DeleteNotebook.js
@@ -1,4 +1,24 @@
+import { deleteNotebook } from '../../store/notebook';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
 function DeleteNotebook ({notebookId}) {
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const notebook = useSelector(state => state.notebook[notebookId]);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch(deleteNotebook(notebookId))
+        history.push('/');
+    }
+
+    return (
+        <form onSubmit={e => handleSubmit(e)}>
+            <h2>Delete Notebook?</h2>
+            <button type="submit">Yes</button>
+        </form>
+    )
 
 }
 
