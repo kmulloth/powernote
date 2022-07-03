a681a2bc356699b43a4018cca1c4b22390e54a62
KMulloth <k.mulloth@gmail.com>
2022-07-03 12:35:42 -0500

    css

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,14 +1,20 @@
 import {useState, useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
 import {getNotes, addNote, editNote, deleteNote} from '../../store/note';
+import {getNotebooks} from '../../store/notebook';
 import NewNoteModal from '../NewNoteModal';
 import './Home.css';
 
 function Home () {
     const dispatch = useDispatch();
+    const history = useHistory();
     const sessionUser = useSelector(state => state?.session?.user);
     const allNotes = useSelector(state => state?.note)
     const userNotes = Object.values(allNotes).filter(note => note?.author_id === sessionUser?.id)
+    const allNotebooks = useSelector(state => state?.notebook)
+    const userNotebooks = Object.values(allNotebooks).filter(notebook => notebook?.author_id === sessionUser?.id)
+    console.log(userNotebooks)
 
     useEffect(() => {
         dispatch(getNotes({include: [{model: 'User'}]}));
@@ -16,7 +22,21 @@ function Home () {
 
     return(
         <div className="home">
-            <h1>Notes</h1>
+            <div className='img-container'>
+                <img src='https://wallpaperaccess.com/full/1282257.jpg' alt="notebook"/>
+            </div>
+            <div className='select-notebooks'>
+                {sessionUser ? userNotebooks.slice(0,5).map(notebook => (
+                    <div key={notebook?.id}>
+                        <button onClick={e => {
+                            history.push(`/notebooks/${notebook?.id}`)
+                        }
+                        }>{notebook?.title}</button>
+                    </div>
+                )) : <div className='logine-signup-prompt'>
+                    <p>Welcome to PwrNote, please login or create an account to continue</p>
+                </div>}
+            </div>
         </div>
     )
 }
